1d12df80270a2c93bead38d955946b30cdae6fae
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-09-14 18:06:09 +0300

    orders are more secure now

--- internal/orders/handler.go
+++ internal/orders/handler.go
@@ -76,6 +76,10 @@ func (handler *Handler) Detail(responseWriter http.ResponseWriter, request *http
 		handler.orderNotFound(responseWriter)
 		return
 	}
+	if order.ID != current.User.ID {
+		handler.orderNotFound(responseWriter)
+		return
+	}
 	orderItems, err := handler.orderStore.ListItems(request.Context(), order.ID)
 	if err != nil {
 		handler.internalError(responseWriter, request, err)
